dbb438ecbc9613d422805068f8942ff7e9bb37cb
AmanKumarSah07 <btech10565.23@bitmesra.ac.in>
2026-04-03 21:39:05 +0530

    Restore fully functional copy from test2 - Copy

--- backend/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-import { AppError } from './errorHandler.js';
-
-export const notFound = (req, res, next) => {
-  const error = new AppError(`Not Found - ${req.originalUrl}`, 404);
-  next(error);
-};
-
-export default notFound;
-
